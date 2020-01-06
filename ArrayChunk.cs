d1cf864b40d92a46ad1e383f1012cd3fc6014660
deadmadara <46846194+deadmadara@users.noreply.github.com>
2020-01-06 23:07:01 +0300

    Add files via upload

---
+++ ArrayChunk.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortSpace
+{
+    public static class SortLevel
+    {
+        public static int ArrayChunk(int[] M)
+        {
+            int pos = M.Length / 2 ;
+            int N = M[pos];
+            int i1 = 0;
+            int i2 = M.Length - 1;
+
+            while (i1 <= i2 - 1)
+            {
+                while (M[i1] < N) i1++;
+                while (M[i2] > N) i2--;
+                
+
+                if (i1 == i2 - 1 && M[i1] > M[i2])
+                {
+                    int temp = M[i1];
+                    M[i1] = M[i2];
+                    M[i2] = temp;
+                    ArrayChunk(M);
+                }
+                if (i1 == i2 || (i1 == i2 - 1 && M[i1] < M[i2])) return pos;
+
+                if (i1 == pos) pos = i1;
+                if (i2 == pos) pos = i2;
+                N = M[pos];
+                int temp1 = M[i1];
+                M[i1] = M[i2];
+                M[i2] = temp1;
+            }
+            return pos;
+        }
+    }
+}
