d02e37b4f1b47744ec6d76045b216486f6414f40
deadmadara <46846194+deadmadara@users.noreply.github.com>
2020-01-11 17:27:10 +0300

    Update ArrayChunk.cs

--- ArrayChunk.cs
+++ ArrayChunk.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace SortSpace
@@ -7,17 +7,15 @@ namespace SortSpace
     {
         public static int ArrayChunk(int[] M)
         {
-            int pos = M.Length / 2 ;
+            int pos = M.Length / 2;
             int N = M[pos];
             int i1 = 0;
             int i2 = M.Length - 1;
 
-            while (i1 <= i2 - 1)
+            while (i1 <= i2 )
             {
                 while (M[i1] < N) i1++;
                 while (M[i2] > N) i2--;
-                
-
                 if (i1 == i2 - 1 && M[i1] > M[i2])
                 {
                     int temp = M[i1];
@@ -25,14 +23,16 @@ namespace SortSpace
                     M[i2] = temp;
                     ArrayChunk(M);
                 }
-                if (i1 == i2 || (i1 == i2 - 1 && M[i1] < M[i2])) return pos;
-
-                if (i1 == pos) pos = i1;
-                if (i2 == pos) pos = i2;
-                N = M[pos];
-                int temp1 = M[i1];
-                M[i1] = M[i2];
-                M[i2] = temp1;
+                else if (i1 == i2 || (i1 == i2 - 1 && M[i1] < M[i2])) return pos;
+                else
+                {
+                    if (i1 == pos) pos++;
+                    if (i2 == pos) pos--;
+                    N = M[pos];
+                    int temp1 = M[i1]; 
+                    M[i1] = M[i2];
+                    M[i2] = temp1;
+                }
             }
             return pos;
         }
