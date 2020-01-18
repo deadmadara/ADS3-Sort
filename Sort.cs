d913e0b6502e74b5f3034e38f00a2d683f6fe232
deadmadara <46846194+deadmadara@users.noreply.github.com>
2020-01-18 21:45:41 +0300

    Add files via upload

---
+++ Sort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortSpace
+{
+    public static class SortLevel
+    {
+        public static int ArrayChunkPartition(int[] array, int start, int end)
+        {
+            int marker = start;
+            for (int i = start; i <= end; i++)
+            {
+                if (array[i] <= array[end])
+                {
+                    int temp = array[marker]; // swap
+                    array[marker] = array[i];
+                    array[i] = temp;
+                    marker += 1;
+                }
+            }
+            return marker - 1;
+        }
+        public static List<int> KthOrderStatisticsStep(int[] Array, int L, int R, int k)
+        {
+            List<int> pair = new List<int>();
+            int N = ArrayChunkPartition(Array, L, R);
+
+            if (N == k)
+            {
+                pair.Add(L);
+                pair.Add(R);
+            }
+            else if (N < k)
+            {
+                pair.Add(N + 1);
+                pair.Add(R);
+            }
+            else
+            {
+                pair.Add(L);
+                pair.Add(N - 1);
+            }
+            return pair;
+        }
+    }
+}
