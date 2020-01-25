401287168fe6a2b88b2c4480d5f90c716d5fc25f
deadmadara <46846194+deadmadara@users.noreply.github.com>
2020-01-25 14:13:41 +0300

    Update Sort.cs

--- Sort.cs
+++ Sort.cs
@@ -1,29 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace SortSpace
 {
     public static class SortLevel
     {
-        public static int ArrayChunkPartition(int[] array, int start, int end)
-        {
-            int marker = start;
-            for (int i = start; i <= end; i++)
-            {
-                if (array[i] <= array[end])
-                {
-                    int temp = array[marker]; // swap
-                    array[marker] = array[i];
-                    array[i] = temp;
-                    marker += 1;
-                }
-            }
-            return marker - 1;
-        }
         public static List<int> KthOrderStatisticsStep(int[] Array, int L, int R, int k)
         {
             List<int> pair = new List<int>();
-            int N = ArrayChunkPartition(Array, L, R);
+            int N = ArrayChunkTwo(Array, L, R);
 
             if (N == k)
             {
@@ -41,6 +26,38 @@ namespace SortSpace
                 pair.Add(N - 1);
             }
             return pair;
+        }
+
+        public static int ArrayChunkTwo(int[] M, int left, int right)
+        {
+            int pos = (right + left) / 2;
+            int N = M[pos];
+            int i1 = left;
+            int i2 = right;
+
+            while (i1 <= i2)
+            {
+                N = M[pos];
+                while (M[i1] < N && i1 <= right) i1++;
+                while (M[i2] > N && i2 >= left) i2--;
+                if (i1 == i2 - 1 && M[i1] > M[i2])
+                {
+                    int temp = M[i1];
+                    M[i1] = M[i2];
+                    M[i2] = temp;
+                    ArrayChunkTwo(M, left, right);
+                }
+                else if (i1 == i2 || (i1 == i2 - 1 && M[i1] < M[i2])) return pos;
+                else
+                {
+                    if (i1 == pos) pos = i2;
+                    else if (i2 == pos) pos = i1;
+                    int temp1 = M[i1];
+                    M[i1] = M[i2];
+                    M[i2] = temp1;
+                }
+            }
+            return pos;
         }
     }
 }
