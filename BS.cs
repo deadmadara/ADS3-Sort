3a06e49a24a46dc848f66d90e38502003abcc2bf
deadmadara <46846194+deadmadara@users.noreply.github.com>
2020-03-11 20:12:28 +0300

    Add files via upload

---
+++ BS.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortSpace
+{
+    public class BinarySearch
+    {
+        public int Left;
+        public int Right;
+        int flag;
+        int[] array;
+
+        public BinarySearch (int[] arr)
+        {
+            array = new int[arr.Length];
+            Array.Copy(arr, array, arr.Length);
+            Left = 0;
+            Right = arr.Length - 1;
+        }
+
+        public void Step (int N)
+        {
+            if (flag == 0)
+            {
+                int Center = (Left + Right) / 2;
+                if (array[Center] == N)
+                {
+                    flag = 1;
+                }
+                else
+                {
+                    if (Left == Right)
+                    {
+                        flag = -1;
+                    }
+                    else
+                    {
+                        if (N < array[Center])
+                        {
+                            Right = Center - 1;
+                        }
+                        if (N > array[Center])
+                        {
+                            Left = Center + 1;
+                        }
+                        if (Right < 0 || Left > array.Length - 1)
+                        {
+                            flag = -1;
+                        }
+                        Center = (Left + Right) / 2;
+                        if (array[Center] == N)
+                        {
+                            flag = 1;
+                        }
+                        else if (Left == Right)
+                        {
+                            flag = -1;
+                        }
+                    }
+                }
+            }
+        }
+
+        public int GetResult ()
+        {
+            return flag;
+        }
+    }
+}
